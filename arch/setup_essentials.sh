21d1d7b5735a9486d19fd083dd792489cf99641f
Sembarang <agilfikriawan2803@gmail.com>
2025-01-29 16:41:55 +0800

    update fix arch essentials script

--- arch/setup_essentials.sh
+++ arch/setup_essentials.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "I USE ARCH BTW!!!\n"
+echo "I USE ARCH BTW!!!\n"
 
 echo -e "\nUpdating Arch...\n"
 sudo pacman -Syu --noconfirm
@@ -14,6 +14,9 @@ sudo chsh -s $(which fish)
 echo -e "\nInstalling CURL...\n"
 sudo pacman -S curl --noconfirm
 
+echo -e "\nInstalling wget\n"
+sudo pacman -S wget --noconfirm
+
 echo -e "\nInstalling Zoxide...\n"
 sudo apt pacman -S zoxide --noconfirm
 
@@ -21,11 +24,23 @@ echo -e "\nInstalling Git...\n"
 sudo pacman -S git --noconfirm
 git --version
 
+# if you want to install google chrome from AUR uncomment this line
+# echo "\nInstalling Google Chrome\n"
+# git clone https://aur.archlinux.org/google-chrome.git
+# cd google-chrome
+# makepkg -si --noconfirm
+# cd ..
+# rm -rf google-chrome
+
 echo -e "\nInstalling Visual Studio Code...\n"
-sudo pacman -S code 
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+cd visual-studio-code-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf visual-studio-code-bin
 
 echo -e "\nInstalling Neovim...\n"
 sudo pacman -S neovim --noconfirm
 nvim --version
 
-echo -e "\nReboot your system if you want to change default shell to fish\n"
+echo "\nReboot your system if you want to change default shell to fish\n"
